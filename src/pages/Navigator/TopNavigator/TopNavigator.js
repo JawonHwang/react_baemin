59d189bb98ccaf3a35a3ccf460bec5cfc0e41b86
JawonHwang <jackie4386@naver.com>
2024-08-25 13:18:37 +0900

    회원접속 중간 commit

--- src/pages/Navigator/TopNavigator/TopNavigator.js
+++ src/pages/Navigator/TopNavigator/TopNavigator.js
@@ -2,6 +2,7 @@ import styles from "./TopNavigator.module.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Row, Col } from "reactstrap";
 import React from "react";
+import { Menubar } from 'primereact/menubar';
 
 const TopNavigator = () => {
 
@@ -10,17 +11,48 @@ const TopNavigator = () => {
             <Row>
                 <Col className={styles.header_left}>
                     <Row>
-                        left
+                        <Menubar model={items} />
                     </Row>
                 </Col>
                 <Col className={styles.header_center}>
                     <Row>
-                       center
+                        빈칸
                     </Row>
                 </Col>
                 <Col className={styles.header_right}>
                     <Row>
-                        right
+                        {
+                            loginID ?
+                                <>
+                                    <Col>
+                                        <Link className={styles.linkurl} to="/"><div className={activeLink === '' ? styles.activeLink : styles.linkurl} onClick={() => handleLinkClick('')}>BADMINTON</div></Link>
+                                    </Col>
+                                    <Col>
+                                        <Link className={styles.linkurl} to="/Board"><div className={activeLink === 'Board' ? styles.activeLink : styles.linkurl} onClick={() => handleLinkClick('Board')}>BOARD</div></Link>
+                                    </Col>
+                                    <Col>
+                                        <Link className={styles.linkurl} to={`/Calendar`}><div>CALENDAR</div></Link>
+                                    </Col>
+                                    <Col>
+                                        <Link className={styles.linkurl} to={`/Community`}><div>COMMUNITY</div></Link>
+                                    </Col>
+                                    <Col>
+                                        <Link className={styles.linkurl} to={`/MyPage`}><div>MY PAGE</div></Link>
+                                    </Col>
+                                    <Col>
+                                        <div className={styles.linkurl} onClick={handleLogoutClick}><div className={activeLink === 'logout' ? styles.activeLink : styles.linkurl} onClick={() => handleLinkClick('logout')}>LOGOUT</div></div>
+                                    </Col>
+                                </>
+                                :
+                                <>
+                                    <Col>
+                                        <div className={styles.linkurl} onClick={handleLoginClick}><div className={activeLink === 'login' ? styles.activeLink : styles.linkurl} onClick={() => handleLinkClick('login')}>LOGIN</div></div>
+                                    </Col>
+                                    <Col>
+                                        <Link className={styles.linkurl} to="/Register"><div className={activeLink === 'Register' ? styles.activeLink : styles.linkurl} onClick={() => handleLinkClick('Register')}>REGISTER</div></Link>
+                                    </Col>
+                                </>
+                        }
                     </Row>
                 </Col>
             </Row>
